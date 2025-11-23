97525f09d34f3b092f588135ea36dd668b66f26c
Philip Roddenberry <philip@noggn.com>
2025-11-22 23:22:12 -0800

    Remove Vue routing and default components

--- src/main.js
+++ src/main.js
@@ -4,17 +4,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Nora from '@primeuix/themes/nora'
-import Aura from '@primeuix/themes/aura'
 import Button from 'primevue/button'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
 
 app.use(PrimeVue, {
   theme: {
